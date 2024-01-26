f7ef3b798efd6044c30ff8bacf8a7ee7ca54aa1b
akshayp-mangospring <akshayp@mangospring.com>
2024-01-26 22:01:05 +0530

    Add a basic interval based German intro shell script

---
+++ code/bash/interval_based_echos.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Hallo, ich bin Akshay."
+sleep 2
+echo "Freut mich. Wie geht es Ihnen?"
+sleep 2
+echo "Danke! Ich bin gut."
+sleep 2
+echo "Ich muss schneller zur Arbeit gehen. Bis Bald. Tschüss!"
