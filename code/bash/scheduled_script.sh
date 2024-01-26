1010b7490fd9ab25f95851118bf6c7f88e2cd736
akshayp-mangospring <akshayp@mangospring.com>
2024-01-26 22:01:54 +0530

    Add basic info about scheduling scripts and a basic script of it

---
+++ code/bash/scheduled_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# This script was run using the `at` command to open up our daily standup meet a minute after when this script was written.
+brave-browser https://meet.google.com/pkg-nxri-rwq?pli=1
